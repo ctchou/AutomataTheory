fdf648953077ef95c41f5143870896758463bf90
Ching-Tsun Chou <chingtsun.chou@gmail.com>
2025-04-28 13:42:48 -0700

    Add AutomataTheory/AutomataBasic.lean

--- AutomataTheory.lean
+++ AutomataTheory.lean
@@ -1,4 +1,5 @@
 -- This module serves as the root of the `AutomataTheory` library.
 -- Import modules here that should be built as part of the library.
 import AutomataTheory.Sequences
+import AutomataTheory.AutomataBasic
 import AutomataTheory.Test
---
+++ AutomataTheory/AutomataBasic.lean
@@ -0,0 +1,52 @@
+/-
+Copyright (c) 2025-present Ching-Tsun Chou All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Ching-Tsun Chou
+-/
+
+import Mathlib.Data.Set.Card
+import Mathlib.Data.List.OfFn
+import Mathlib.Order.Filter.ATTopBot.Basic
+
+open BigOperators Function Set Filter
+
+section AutomataBasic
+
+class Automata (A : Type*) where
+  State : Type*
+  init : Set State
+  next : State → A → Set State
+
+variable {A : Type*}
+
+def Deterministic (M : Automata A) : Prop :=
+  M.init.ncard = 1 ∧ ∀ s a, (M.next s a).ncard = 1
+
+def FinRun (M : Automata A) (n : ℕ) (as : Fin n → A) (ss : Fin (n + 1) → M.State) :=
+  ss 0 ∈ M.init ∧ ∀ k : Fin n, ss (k + 1) ∈ M.next (ss k) (as k)
+
+def InfRun (M : Automata A) (as : ℕ → A) (ss : ℕ → M.State) :=
+  ss 0 ∈ M.init ∧ ∀ k : ℕ, ss (k + 1) ∈ M.next (ss k) (as k)
+
+def FinAccept (M : Automata A) (acc : Set M.State) (n : ℕ) (as : Fin n → A) :=
+  ∃ ss : Fin (n + 1) → M.State, FinRun M n as ss ∧ ss n ∈ acc
+
+def BuchiAccept (M : Automata A) (acc : Set M.State) (as : ℕ → A) :=
+  ∃ ss : ℕ → M.State, InfRun M as ss ∧ ∃ᶠ k in atTop, ss k ∈ acc
+
+def MullerAccept (M : Automata A) (accSet : Set (Set M.State)) (as : ℕ → A) :=
+  ∃ ss : ℕ → M.State, InfRun M as ss ∧ ∃ acc ∈ accSet, ∀ s, s ∈ acc ↔ (∃ᶠ k in atTop, ss k = s)
+
+def RabinAccept (M : Automata A) (accPairs : Set (Set M.State × Set M.State)) (as : ℕ → A) :=
+  ∃ ss : ℕ → M.State, InfRun M as ss ∧ ∃ pair ∈ accPairs, (∃ᶠ k in atTop, ss k ∈ pair.1) ∧ (∀ᶠ k in atTop, ss k ∉ pair.2)
+
+def StreettAccept (M : Automata A) (accPairs : Set (Set M.State × Set M.State)) (as : ℕ → A) :=
+  ∃ ss : ℕ → M.State, InfRun M as ss ∧ ∀ pair ∈ accPairs, (∃ᶠ k in atTop, ss k ∈ pair.1) → (∃ᶠ k in atTop, ss k ∈ pair.2)
+
+def AcceptedLang (M : Automata A) (acc : Set M.State) : Set (List A) :=
+  { al | ∃ n as, FinAccept M acc n as ∧ al = List.ofFn as }
+
+def AcceptedOmegaLang (M : Automata A) (acc : Set M.State) : Set (ℕ → A) :=
+  { as | BuchiAccept M acc as }
+
+end AutomataBasic
