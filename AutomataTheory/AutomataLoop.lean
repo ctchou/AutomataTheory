db94b9f3d22d83744ab4712bc03f378699c7ac54
Ching-Tsun Chou <chingtsun.chou@gmail.com>
2025-05-23 15:09:44 -0700

    Work on AutomataTheory/AutomataLoop.lean

--- AutomataTheory/AutomataLoop.lean
+++ AutomataTheory/AutomataLoop.lean
@@ -46,7 +46,7 @@ theorem automata_loop_fin_accept {m : ℕ} {as : ℕ → A} :
       let m' := Nat.findGreatest loop (m - 1)
       obtain ⟨h_m', h_notM'⟩ : m' < m ∧ ss (m' + 1) ∉ M.next (ss m') (as m') := by
         obtain ⟨k, h_k⟩ := h_loop
-        exact Nat.findGreatest_spec (by omega : k ≤ m - 1) h_k
+        exact Nat.findGreatest_spec (show k ≤ m - 1 by omega) h_k
       have h_next' := h_run.2 m' h_m'
       simp [AutomataLoop, h_notM'] at h_next'
       obtain ⟨h_acc', s0, h_s0_init, h_s0_next⟩ := h_next'
@@ -56,56 +56,45 @@ theorem automata_loop_fin_accept {m : ℕ} {as : ℕ → A} :
         · simpa
         · intro k h_k
           rcases Classical.em (k = 0) with h_k' | h_k'
-          · have h0 : 1 + m' = m' + 1 := by omega
-            simpa [SuffixFrom, h_k', h0]
+          · simpa [SuffixFrom, h_k', (show 1 + m' = m' + 1 by omega)]
           · have h_next : ss (k + m' + 1) ∈ M.next (ss (k + m')) (as (k + m')) := by
-              have h0 := Nat.findGreatest_is_greatest (by omega : m' < k + m') (by omega)
-              simp [loop, (by omega : k + m' < m)] at h0
+              have h0 := Nat.findGreatest_is_greatest (show m' < k + m' by omega) (by omega)
+              simp [loop, (show k + m' < m by omega)] at h0
               exact h0
-            have h1 : k + 1 + m' = k + m' + 1 := by omega
-            simpa [SuffixFrom, h_k', h1]
-        · have h0 : m - m' ≠ 0 := by omega
-          have h1 : m - m' + m' = m := by omega
-          simpa [h0, h1]
+            simpa [SuffixFrom, h_k', (show k + 1 + m' = k + m' + 1 by omega)]
+        · simpa [(show m - m' ≠ 0 by omega), (show m - m' + m' = m by omega)]
       have h_run' : FinRun (AutomataLoop M acc) m' as ss := by
-        sorry
-      sorry
+        constructor
+        · exact h_run.1
+        intro k h_k ; exact h_run.2 k (by omega)
+      obtain ⟨n', φ', h_mono', h_φ'_0, h_φ'_n', h_accept'⟩ := h_ind m' h_m' h_run' h_acc'
+      use (n' + 1), (fun k ↦ if k ≤ n' then φ' k else m)
+      simp [h_φ'_0] ; constructor
+      · apply monotone_nat_of_le_succ ; intro k
+        rcases (show k < n' ∨ k = n' ∨ k > n' by omega) with h_k | h_k | h_k
+        · simp [(show k ≤ n' by omega), (show k + 1 ≤ n' by omega)]
+          exact h_mono' (show k ≤ k + 1 by omega)
+        · simp [h_k, h_φ'_n'] ; omega
+        · simp [(show ¬ k ≤ n' by omega), (show ¬ k + 1 ≤ n' by omega)]
+      · intro k h_k
+        rcases (show k < n' ∨ k = n' by omega) with h_k' | h_k'
+        · simp [(show k ≤ n' by omega), (show k + 1 ≤ n' by omega)]
+          exact h_accept' k h_k'
+        · simpa [h_k', h_φ'_n']
     · simp [loop] at h_loop
-      sorry
-
-    -- rcases Classical.em (∃ k < m, k > 0 ∧ ss (k + 1) ∉ M.next (ss k) (as k)) with h_loop | h_loop
-    -- · obtain ⟨h_n, h_n_pos, h_n_notM⟩ := Nat.find_spec h_loop
-    --   -- have h_run' : FinRun M (Nat.find h_loop) as ss := by
-    --   --   constructor
-    --   --   · exact h_run.1
-    --   --   intro k h_k
-    --   --   have h_min := Nat.find_min h_loop h_k
-    --   --   simp [(by omega : k < m)] at h_min
-    --   --   exact h_min
-    --   have h_next_n := h_run.2 (Nat.find h_loop) h_n
-    --   simp [AutomataLoop, h_n_notM] at h_next_n
-    --   obtain ⟨h_acc', s0, h_s0_init, h_s0_next⟩ := h_next_n
-    --   sorry
-    -- · simp at h_loop
-    --   use (fun k ↦ if k = 0 then 0 else m), 1
-    --   simp ; constructor
-    --   · apply monotone_nat_of_le_succ ; intro k
-    --     rcases Nat.eq_zero_or_eq_succ_pred k with h_k | h_k <;> rw [h_k] <;> simp
-    --   · rcases Nat.eq_zero_or_pos m with h_m | h_m
-    --     · use ss
-    --       simp [FinRun, h_m]
-    --       exact h_run.1
-    --     · obtain ⟨s0, h_s0_init, h_s0_next⟩ := automata_loop_fin_run_1 h_m h_run
-    --       use (fun k ↦ if k = 0 then s0 else ss k)
-    --       constructor
-    --       · simpa
-    --       intro k h_k_m
-    --       rcases Nat.eq_zero_or_pos k with h_k | h_k
-    --       · simpa [h_k]
-    --       · have h_k' : k ≠ 0 := by omega
-    --         simp [h_k']
-    --         exact h_loop k h_k_m h_k
-
+      use (if m = 0 then 0 else 1), (fun k ↦ if k = 0 then 0 else m)
+      constructor
+      · apply monotone_nat_of_le_succ ; intro k
+        rcases (show k = 0 ∨ k ≠ 0 by omega) with h_k | h_k <;> simp [h_k]
+      rcases (show m = 0 ∨ m ≠ 0 by omega) with h_0 | h_1
+      · simp [h_0]
+      · simp [h_1]
+        use ss ; simp [h_acc]
+        constructor
+        · have h_init := h_run.1
+          simp [AutomataLoop] at h_init
+          exact h_init
+        · exact h_loop
   · sorry
 
 end AutomataLoop
