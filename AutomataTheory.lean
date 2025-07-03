131edb48b75b9ed207b2008fc6e08e66726ea785
Ching-Tsun Chou <chingtsun.chou@gmail.com>
2025-07-03 09:34:01 -0700

    Add AutomataTheory/Congruences.lean

--- AutomataTheory.lean
+++ AutomataTheory.lean
@@ -7,6 +7,7 @@ import AutomataTheory.AutomataOI2
 import AutomataTheory.AutomataPSet
 import AutomataTheory.AutomataProd
 import AutomataTheory.AutomataSum
+import AutomataTheory.Congruences
 import AutomataTheory.Languages
 import AutomataTheory.OmegaRegLang
 import AutomataTheory.RegLang
