0ce9494fca173e1d365bd91032c2f0c5ad19a85a
Ching-Tsun Chou <chingtsun.chou@gmail.com>
2025-07-05 18:33:30 -0700

    Rename AutomataPSet to Automaton.PSet

--- AutomataTheory/AutomataPSet.lean
+++ AutomataTheory/AutomataPSet.lean
@@ -19,25 +19,25 @@ section AutomataPSet
 
 variable {A : Type*}
 
-def AutomataPSet (M : Automaton A) : DetAutomaton A where
+def Automaton.PSet (M : Automaton A) : DetAutomaton A where
   State := Set M.State
   init := M.init
   next := fun ps a ↦ ⋃ s ∈ ps, M.next s a
 
 variable {M : Automaton A}
 
-instance : Membership M.State (AutomataPSet M).State := by exact { mem := fun s ↦ s }
+instance : Membership M.State M.PSet.State := by exact { mem := fun s ↦ s }
 
 theorem automata_pset_reach_init (as : ℕ → A) :
-    MakeDetRun (AutomataPSet M) as 0 = M.init := by
-  simp [MakeDetRun, AutomataPSet]
+    MakeDetRun M.PSet as 0 = M.init := by
+  simp [MakeDetRun, Automaton.PSet]
 
 theorem automata_pset_reach_next (as : ℕ → A) (k : ℕ) :
-    MakeDetRun (AutomataPSet M) as (k + 1) = ⋃ s ∈ MakeDetRun (AutomataPSet M) as k, M.next s (as k) := by
-  simp [MakeDetRun, AutomataPSet]
+    MakeDetRun M.PSet as (k + 1) = ⋃ s ∈ MakeDetRun M.PSet as k, M.next s (as k) := by
+  simp [MakeDetRun, Automaton.PSet]
 
 theorem automata_pset_run (as : ℕ → A) (k : ℕ) :
-    MakeDetRun (AutomataPSet M) as k = { s : M.State | ∃ ss, FinRun M k as ss ∧ ss k = s } := by
+    MakeDetRun M.PSet as k = { s : M.State | ∃ ss, FinRun M k as ss ∧ ss k = s } := by
   induction' k with k h_ind
   · rw [automata_pset_reach_init as, Set.ext_iff]
     intro s ; constructor
@@ -63,29 +63,29 @@ theorem automata_pset_run (as : ℕ → A) (k : ℕ) :
     intro j h_j
     exact h_next j (by omega)
 
+end AutomataPSet
+
 section AcceptedLangPSet
 
 variable {A : Type*} (M : Automaton A) (acc : Set M.State)
 
-def AutomataPSet_Acc : Set (Set M.State) := { sset | ∃ s ∈ sset, s ∈ acc }
+def Automaton.PSet_Acc : Set (Set M.State) := { sset | ∃ s ∈ sset, s ∈ acc }
 
 theorem accepted_lang_pset :
-    AcceptedLang (AutomataPSet M).toAutomaton (AutomataPSet_Acc M acc) = AcceptedLang M acc := by
+    AcceptedLang M.PSet.toAutomaton (M.PSet_Acc acc) = AcceptedLang M acc := by
   ext al ; simp only [AcceptedLang, FinAccept] ; constructor
   · rintro ⟨n, as, ⟨ss', h_run', h_acc'⟩, h_al⟩
     have h_sn' := det_automata_fin_run_unique h_run' n (by omega)
     rw [automata_pset_run] at h_sn'
-    simp [h_sn', AutomataPSet_Acc] at h_acc'
+    simp [h_sn', Automaton.PSet_Acc] at h_acc'
     obtain ⟨sn, ⟨ss, h_run, h_sn⟩, h_acc⟩ := h_acc'
     use n, as ; simp [h_al]
     use ss ; simp [h_run, h_sn, h_acc]
   · rintro ⟨n, as, ⟨ss, h_run, h_sn⟩, h_al⟩
     use n, as ; simp [h_al]
-    use (MakeDetRun (AutomataPSet M) as) ; constructor
+    use (MakeDetRun M.PSet as) ; constructor
     · apply det_automata_fin_run_exists
-    · rw [automata_pset_run, AutomataPSet_Acc]
+    · rw [automata_pset_run, Automaton.PSet_Acc]
       use (ss n) ; simp [h_sn] ; use ss
 
 end AcceptedLangPSet
-
-end AutomataPSet
