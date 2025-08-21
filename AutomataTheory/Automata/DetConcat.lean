5f39c491a6bef8725251d8f86c9c4b09e0f47f65
Ching-Tsun Chou <chingtsun.chou@gmail.com>
2025-08-20 18:12:24 -0700

    Work on AutomataTheory/Automata/DetConcat.lean

--- AutomataTheory/Automata/DetConcat.lean
+++ AutomataTheory/Automata/DetConcat.lean
@@ -18,9 +18,11 @@ J. Comput. System Sci. 8 (1974) 117-141.
 open Function Set Filter
 open Classical
 
+namespace Automata
+
 section AutomataDetConcat
 
-variable {A : Type} (M1 : DetAutomaton A) (acc1 : Set M1.State) (M2 : DetAutomaton A)
+variable {A : Type} (M1 : DA A) (acc1 : Set M1.State) (M2 : DA A)
 
 /-- The concatenation automaton runs M1 in parallel with at most n2 + 2 copies
 of M2, where n2 is the number of states of M2.  (So this construction only
@@ -32,7 +34,7 @@ for a new copy of M2, because it is possible to have two M2 copies to be in the 
 state at the same time.  (The maximum possible number of active copies is n2 + 1.)
 Note that we never need to activate and deactivate the same copy at the same time.
 -/
-noncomputable def DetAutomaton.Concat : DetAutomaton A where
+noncomputable def DA.Concat : DA A where
   State := M1.State × (Fin (Nat.card M2.State + 2) → Option M2.State)
   init := (M1.init, fun _ ↦ none)
   next := fun s a ↦ (M1.next s.1 a, fun i ↦
@@ -65,7 +67,7 @@ as M1 running alone.
 -/
 theorem det_automata_concat_inf_run_1 (as : ℕ → A) (k : ℕ) :
     ((M1.Concat acc1 M2).DetRun as k).1 = M1.DetRun as k := by
-  induction' k with k h_ind <;> simp [DetAutomaton.DetRun]
+  induction' k with k h_ind <;> simp [DA.DetRun]
   · rfl
   simp [← h_ind] ; rfl
 
@@ -79,21 +81,21 @@ theorem det_automata_concat_inf_run_2 (as : ℕ → A) (k : ℕ) (i : Fin (Nat.c
   · contradiction
   have h_next := det_automata_concat_next_2 M1 acc1 M2 ((M1.Concat acc1 M2).DetRun as k) (as k) i
   rcases Option.eq_none_or_eq_some <| ((M1.Concat acc1 M2).DetRun as k).2 i with h_k | ⟨s2', h_k⟩
-  · simp [DetAutomaton.DetRun, h_next, h_k, det_automata_concat_inf_run_1] at h
-    use k ; simp [h.1.1, ← h.2, DetAutomaton.DetRun, instSuffixFrom, SuffixFrom]
+  · simp [DA.DetRun, h_next, h_k, det_automata_concat_inf_run_1] at h
+    use k ; simp [h.1.1, ← h.2, DA.DetRun, instSuffixFrom, SuffixFrom]
   · obtain ⟨j, h_j, h_acc, h_run⟩ := h_ind s2' h_k
-    simp [DetAutomaton.DetRun, h_next, h_k] at h
-    use j ; simp [h_acc, ← h.2, DetAutomaton.DetRun, h_run,
+    simp [DA.DetRun, h_next, h_k] at h
+    use j ; simp [h_acc, ← h.2, DA.DetRun, h_run,
       (show j < k + 1 by omega), (show k + 1 - j = k - j + 1 by omega)]
     congr ; simp [instSuffixFrom, SuffixFrom, (show k - j + j = k by omega)]
 
 variable (accSet2 : Set (Set M2.State))
 
-def DetAutomaton.MullerAcc_Concat : Set (Set (M1.Concat acc1 M2).State) :=
+def DA.MullerAcc_Concat : Set (Set (M1.Concat acc1 M2).State) :=
   { acc | ∃ i, {s2 | ∃ s ∈ acc, s.2 i = some s2} ∈ accSet2 ∧ (∀ s ∈ acc, (s.2 i).isSome) }
 
 theorem det_automata_concat_muller_accept (as : ℕ → A)
-    (h : (M1.Concat acc1 M2).MullerAccept (DetAutomaton.MullerAcc_Concat M1 acc1 M2 accSet2) as) :
+    (h : (M1.Concat acc1 M2).MullerAccept (DA.MullerAcc_Concat M1 acc1 M2 accSet2) as) :
     ∃ k, M1.toNA.FinAccept acc1 k as ∧ M2.MullerAccept accSet2 (as <<< k) := by
   sorry
 
