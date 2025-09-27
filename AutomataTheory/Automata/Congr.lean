c7bcce8c2e162c733ec7b9f3a75391808f2893eb
Ching-Tsun Chou <chingtsun.chou@gmail.com>
2025-09-26 22:33:21 -0700

    Improve the definition of Automata.NA.AcceptedLang

--- AutomataTheory/Automata/Congr.lean
+++ AutomataTheory/Automata/Congr.lean
@@ -38,9 +38,9 @@ variable {c : Congruence A}
 /-- Running `c.toDA` on input `as` ends in state `⟦ as ⟧`.
 -/
 theorem automaton_congr_run (as : ℕ → A) (n : ℕ) :
-    c.toDA.DetRun as n = ⟦ List.ofFn (fun k : Fin n ↦ as k) ⟧ := by
+    c.toDA.DetRun as n = ⟦ as ⇊ 0 n ⟧ := by
   induction' n with n h_ind
-  · simp [Automata.DA.DetRun, Congruence.toDA]
+  · simp [Automata.DA.DetRun, Congruence.toDA, empty_FinSubseq]
   simp only [Automata.DA.DetRun, h_ind]
   simp only [Congruence.toDA, Quotient.lift_mk]
   suffices h_eq : ((List.ofFn fun k : Fin n ↦ as ↑k) ++ [as n]) = (List.ofFn fun k : Fin (n + 1) ↦ as ↑k) by simp [h_eq]
@@ -57,11 +57,11 @@ theorem acc_lang_congr [Inhabited A] (s : c.toDA.State) :
     simp [h_ss_n, automaton_congr_run, Congruence.EqvCls]
   · rintro ⟨rfl⟩
     let as := fun k ↦ if h : k < al.length then al[k] else default
-    use al.length, as
-    constructor <;> [skip ; simp [as]]
-    use (c.toDA.DetRun as)
-    constructor
+    have h1 : as ⇊ 0 al.length = al := by
+      ext k a ; simp [instFinSubseq, FinSubseq] ; grind
+    use al.length, as ; simp [h1]
+    use (c.toDA.DetRun as) ; constructor
     · exact Automata.da_fin_run_exists al.length as
-    · simp [automaton_congr_run, as]
+    · simp [automaton_congr_run, h1]
 
 end AutomataCongr
